44837d1bad721d28865166c9a36de6d900b92c2a
wuyou2008 <2936978448@qq.com>
2026-05-19 08:21:32 +0800

    delpoy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
+  site: 'https://kuyy29666.github.io',
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
